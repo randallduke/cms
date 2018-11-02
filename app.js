c738ed146a683a5d576f1be19c96f4cf746bf6fb
randallduke <rrtd15@gmail.com>
2018-11-02 16:04:54 -0700

    Lots o changes

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'home'}));
+app.set('view engine', 'handlebars');
+
+const main = require('./routes/home/main');
+
+app.use('/', main);
 
 app.listen(4500, ()=>{
 
