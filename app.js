89e4ab1120881394f1a6ee199685729921896a42
randallduke <rrtd15@gmail.com>
2018-11-03 12:58:25 -0700

    Frustrated with Node

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
+//connect to database
+mongoose.connect('mongodb://localhost:27017/cms', {useNewUrlParser: true}).then((db)=>{
+
+    console.log('MONGO connected');
+
+}).catch(error=> console.log("DB Connection Failed" + error));
+
+
+//dynamic paths
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Set View engine
@@ -13,11 +23,13 @@ app.set('view engine', 'handlebars');
 //load routes
 const home = require('./routes/home/index');
 const admin = require('./routes/admin/index');
+const posts = require('./routes/admin/posts');
 
 
 //use routes
 app.use('/', home);
 app.use('/admin', admin);
+app.use('/admin/posts', posts);
 
 app.listen(4500, ()=>{
 
---
+++ routes/admin/posts.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.all('/*', (req, res, next)=>{
+
+    req.app.locals.layout = 'admin';
+    next();
+
+
+});
+
+
+
+router.get('/', (req, res)=>{
+
+    res.send('it work');
+
+});
+
+router.get('/create', (req, res)=>{
+
+    res.render('admin/posts/create');
+
+});
+
+
+router.get('/test', (req, res)=>{
+
+    res.render('admin/posts/test');
+
+});
+
+
+module.exports = router;
